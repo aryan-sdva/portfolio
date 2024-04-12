8d017eb1018bf2378afa6fe677b5d1574e4e1744
Aryan Sachdeva <sachdevaaryan00@gmail.com>
2024-04-13 03:09:00 +0530

    logo packages

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,4 @@
 import { Box, Button, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react"; 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons"; 
 import React from "react"; 
 import { Link } from "react-router-dom";
 import '../styles/cards.css';
